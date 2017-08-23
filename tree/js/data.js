d362f92f27256bc659e896ae88233a5aeeee550f
Benq2088 <31240784+Benq2088@users.noreply.github.com>
2017-08-23 17:45:35 +0800

    简单的数状列表实现

---
+++ tree/js/data.js
@@ -0,0 +1,92 @@
+var treeData = [
+    {
+    	name : "一级列表1",
+        id : "0",
+        children : [
+           {
+        	   name : "二级列表0-0",
+               id : "0-0",
+               children : [
+					{
+						name : "三级列表标题1",
+					    id : "2-0",
+					    children : []
+					},
+					{
+						name : "三级列表标题2",
+					    id : "2-2",
+					    children : [
+					       {
+								name : "四级列表标题1",
+							    id : "3-0",
+							    children : []
+							},{
+								name : "四级列表标题2",
+							    id : "3-0",
+							    children : []
+							}
+					    ]
+					},
+					{
+						name : "三级列表标题3",
+					    id : "2-1",
+					    children : []
+					}
+               ]
+           },
+           {
+        	   name : "二级列表0-1",
+               id : "0-1",
+               children : []
+           },
+           {
+        	   name : "二级列表0-2",
+               id : "0-2",
+               children : []
+           }
+        ]
+    },
+    {
+    	name : "一级列表2",
+        id : "1",
+        children : [
+           {
+        	   name : "二级列表",
+               id : "1-0",
+               children : []
+           }
+        ]
+    },
+    {
+    	name : "一级列表3",
+        id : "2",
+        children : [
+           {
+        	   name : "二级列表",
+               id : "2-0",
+               children : []
+           }
+        ]
+    },
+    {
+    	name : "一级列表4",
+        id : "3",
+        children : [
+           {
+        	   name : "二级列表",
+               id : "3-0",
+               children : []
+           }
+        ]
+    },
+    {
+    	name : "一级列表5",
+        id : "4",
+        children : []
+    },
+    {
+    	name : "一级列表6",
+        id : "5",
+        children : []
+    }
+]
